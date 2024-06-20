fb351ee430920181d4f04ba930500c275081cd7c
quanghung306 <94045688+quanghung306@users.noreply.github.com>
2024-06-20 13:27:30 +0700

    comiit

---
+++ routes/stripe.js
@@ -0,0 +1,159 @@
+const express = require("express");
+const Stripe = require("stripe");
+const product = require("../models/product");
+require("dotenv").config();
+const stripe = Stripe(process.env.STRIPE_KEY);
+const router = express.Router();
+
+router.post("/create-checkout-session", async (req, res) => {
+  const customer = await stripe.customers.create({
+    metadata: {
+      userId: req.body.userId,
+      cart: JSON.stringify(req.body.cartItems),
+    },
+  });
+  const line_items = req.body.cartItems.map((item) => {
+    return {
+      price_data: {
+        currency: "VND",
+        product_data: {
+          name: item.title,
+          images: [item.image],
+          description: item.description,
+          metadata: {
+            id: item.id,
+          },
+        },
+        unit_amount: item.price,
+      },
+      quantity: item.cartQuantity,
+    };
+  });
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    shipping_address_collection: {
+      allowed_countries: ["VN"],
+    },
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          type: "fixed_amount",
+          fixed_amount: {
+            amount: 0,
+            currency: "VND",
+          },
+          display_name: "Free shipping",
+          // Delivers between 5-7 business days
+          delivery_estimate: {
+            minimum: {
+              unit: "business_day",
+              value: 5,
+            },
+            maximum: {
+              unit: "business_day",
+              value: 7,
+            },
+          },
+        },
+      },
+      {
+        shipping_rate_data: {
+          type: "fixed_amount",
+          fixed_amount: {
+            amount: 15000,
+            currency: "VND",
+          },
+          display_name: "Next day air",
+          // Delivers in exactly 1 business day
+          delivery_estimate: {
+            minimum: {
+              unit: "business_day",
+              value: 1,
+            },
+            maximum: {
+              unit: "business_day",
+              value: 1,
+            },
+          },
+        },
+      },
+    ],
+    phone_number_collection: {
+      enabled: true,
+    },
+    customer: customer.id,
+    line_items,
+    mode: "payment",
+    success_url: `${process.env.CLIENT_URL}/checkout-success`,
+    cancel_url: `${process.env.CLIENT_URL}/store`,
+  });
+
+  res.send({ url: session.url });
+});
+//createOrder
+const createOrder =async(customer,data)=>{
+  const Items =JSON.parse(customer.metadata.cart)
+  const newOrder = new Order({
+    userId:customer.metadata.userId,
+    customerId:data.customer,
+    paymentIntentId:data.payment_intent,
+    products:Items,
+    subtotal:data.amount_subtotal,
+    total:data.amount_total,
+    shipping:data.customer_details,
+    payment_status:data.payment_status,
+  }) ;
+  try {
+    const saveOrder = await newOrder.save()
+    console.log("🚀 ~ createOrder ~ saveOrder:", saveOrder)
+  } catch (e) {
+    console.log("🚀 ~ createOrder ~ e:", e)
+    
+  }
+}
+// stripe webhook
+
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+let endpointSecret;
+// endpointSecret = "whsec_6fe75b127f955d4ba37102e4979526c228dea55a8df489158f04a513e8f9ef44";
+
+router.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  (req, res) => {
+    const sig = req.headers["stripe-signature"];
+    let data;
+    let eventType;
+    if (endpointSecret) {
+      let event;
+      try {
+        event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
+        console.log("Webhook verified");
+      } catch (err) {
+        console.log(`Webhook Error: ${err.message}`);
+        res.status(400).send(`Webhook Error: ${err.message}`);
+        return;
+      }
+      data = event.data.object;
+      eventType = event.type;
+    } else {
+      data = req.body.data.object;
+      eventType = req.body.type;
+    }
+
+    // Handle the event
+    if (eventType === "checkout.session.completed") {
+      stripe.customers
+        .retrieve(data.customer)
+        .then((customer) => {
+          createOrder(customer, data);
+        })
+        .catch((err) => console.log(err.message));
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    res.send().end();
+  }
+);
+
+module.exports = router;
